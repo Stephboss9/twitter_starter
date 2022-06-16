0ecfbb488b225df44943d5a8bf46d7e10067fb57
Stephboss9 <mbengastephane34@gmail.com>
2022-06-15 20:59:10 -0400

    implemented expanded text area stretch feature

--- src/components/TweetBox/TweetInput.jsx
+++ src/components/TweetBox/TweetInput.jsx
@@ -2,11 +2,23 @@ import * as React from "react"
 import AvatarIcon from "../AvatarIcon/AvatarIcon"
 
 export default function TweetInput(props) {
+
+  let handleOnFocus = ()=> {
+    document.querySelector("textarea").classList.add("expanded")
+  }
+
+  let handleOnBlur = () => {
+    if (props.value.length === 0){
+      document.querySelector("textarea").classList.remove("expanded")
+    }
+  }
+
   return (
     <div className="tweet-textarea">
       <AvatarIcon />
 
-      <textarea name="new-tweet-input" type="text" placeholder="What's Happening?" value = {props.value} onChange = {props.handleOnChange}></textarea>
+      <textarea name="new-tweet-input" type="text" placeholder="What's Happening?" value = {props.value} onBlur = {handleOnBlur} 
+      onFocus = {handleOnFocus} onChange = {props.handleOnChange}></textarea>
 
       <SmileIcon />
     </div>
